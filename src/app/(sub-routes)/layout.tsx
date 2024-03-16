c00fc8c678196fb871869d4460e4564f40bdcc3a
martin.foakes <martin.foakes@urbansportsclub.com>
2024-03-16 17:28:09 +0100

    pull day images and workout complete btn

--- src/app/(sub-routes)/layout.tsx
+++ src/app/(sub-routes)/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import confetti from "canvas-confetti";
 
 import NavBar from "@/components/Nav/NavBar";
 import { toTitleCase } from "@/helpers/formatters";
+import { useRouter } from "next/navigation";
 
 const RouteLayout = ({
   children,
@@ -12,6 +14,8 @@ const RouteLayout = ({
   children: React.ReactNode;
 }>) => {
   const [pageName, setPageName] = useState<string | undefined>("");
+  const [completeBtnClick, setCompleteBtnClick] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -25,6 +29,48 @@ const RouteLayout = ({
       <NavBar pageTitle={pageName} />
       <div className="sub-layout-child-container">
         <Container className="workout-day-container">{children}</Container>
+
+        <Container
+          sx={{
+            padding: 2,
+            justifyContent: "center",
+            display: "inline-flex",
+          }}
+        >
+          <Button
+            sx={{
+              padding: "8px 24px",
+              borderRadius: "24px",
+            }}
+            variant="contained"
+            color="success"
+            size="large"
+            disabled={completeBtnClick}
+            disableFocusRipple
+            onClick={() => {
+              setCompleteBtnClick(true);
+              confetti({
+                particleCount: 200,
+                startVelocity: 30,
+                spread: 360,
+                origin: {
+                  x: Math.random(),
+                  y: Math.random() - 0.2,
+                },
+              });
+
+              setTimeout(() => {
+                setCompleteBtnClick(false);
+
+                router.push("/");
+              }, 1000);
+            }}
+          >
+            <Typography variant="h6" component="h6">
+              {"Workout Complete!"}
+            </Typography>
+          </Button>
+        </Container>
       </div>
     </>
   );
--- src/app/(sub-routes)/metadata.ts
+++ src/app/(sub-routes)/metadata.ts
@@ -74,48 +74,63 @@ export const pullDayMeta: Array<{
     primaryText: "Deadlifts",
     setAmount: "3",
     repAmount: "5",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/db0c82d5-e90a-469f-9145-e5370594e7b0/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Pull Ups",
     setAmount: "3",
     repAmount: "Until failure",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/02a8623c-c7a9-4480-afa3-a52d1b7bb819/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Lat Pulldowns",
     setAmount: "3",
     repAmount: "10",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/4a56b60f-112a-4090-b9c0-2bdb4b7891ac/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Pullovers",
     setAmount: "3",
     repAmount: "10-15",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/def0b7e3-4e54-43ed-8cc1-1a680ada92bc/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Dumbbell Rows",
     setAmount: "3",
     repAmount: "10",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/7b4bd650-1a0b-49ad-bef0-ec4f1331c6e9/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Cable Face Pulls",
     setAmount: "3",
     repAmount: "12",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/e65e714c-b8fb-4c7c-8633-d232a4ee71fa/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Barbell/Dumbbell Curls",
     setAmount: "3",
     repAmount: "10",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/0d4cbf81-19e6-4ce2-ba5b-990ba801a5ef/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+  },
+  {
+    primaryText: "Cable Curls",
+    setAmount: "3",
+    repAmount: "10",
+    imgUrl:
+      "https://i.shgcdn.com/16f1da3e-1670-466b-98ff-4a18143c63dd/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
   {
     primaryText: "Preacher Curls",
     setAmount: "3",
     repAmount: "12-15",
-    imgUrl: "",
+    imgUrl:
+      "https://i.shgcdn.com/a382e76d-6ca2-4275-b346-9e83dae0223a/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
   },
 ];
